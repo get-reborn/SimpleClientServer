9dd284db1413d3d4a4cc331b75302f1114e97f63
reget <re.masterzero@gmail.com>
2020-12-26 16:22:05 +0800

    init error msg

--- SocketErr.cpp
+++ SocketErr.cpp
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <winsock2.h>
+#include "SocketErr.h"
 
 void errexit(const char *format, ...) {
-    va_list  args;
+    va_list args;
     va_start(args, format);
     vfprintf(stderr, format, args);
     va_end(args);
-    WSACleanup();
-    exit(1);
+    //WSACleanup();
 }
